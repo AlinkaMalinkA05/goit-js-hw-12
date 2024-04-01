b186632f9fad531b8e24020ba6f181029f956364
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-04-01 21:05:49 +0200

    create async functoin

--- src/js/pixaby-api.js
+++ src/js/pixaby-api.js
@@ -1,24 +1,25 @@
-export function getPicture(query) {
-  const baseUrl = 'https://pixabay.com/api/';
-  const keyApi = '43059548-3adc942b003e7790296d060a7';
+import axios from "axios";
 
-  const queryParams = new URLSearchParams({
+export async function getPicture(query) {
+const baseUrl = 'https://pixabay.com/api/';
+const keyApi = '43059548-3adc942b003e7790296d060a7';
+
+const queryParams = new URLSearchParams({
     key: keyApi,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
     per_page: 40,
-  });
-
-    return fetch(`${baseUrl}?${queryParams}`).then(res => {
+});
+try { const res =  await fetch(`${baseUrl}?${queryParams}`)
         if (!res.ok) {
             throw new Error("error");
-        }
-        return res.json();
-    })
-        .then((data) => {
-            console.log(data)
-            return data
-        }).catch((error) => console.log("error"));
+    } 
+    const data = await res.json();
+    console.log(data)
+    return data;
+} catch(error) {
+    console.log("error");
+    } 
 }
