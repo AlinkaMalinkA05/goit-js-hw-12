f2dabb14f3cfba9eb02bb528bbe44c3df00b23bf
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-04-01 20:31:26 +0200

    aded biblioteks

---
+++ src/js/render-functions.js
@@ -0,0 +1,9 @@
+import { getPicture } from "./pixaby-api";
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+// Описаний у документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
