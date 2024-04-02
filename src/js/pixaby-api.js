29f704139bd07bd42a74f82ca6c8bc3dba69738b
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-04-02 22:52:57 +0200

    nothing added

--- src/js/pixaby-api.js
+++ src/js/pixaby-api.js
@@ -1,25 +1,23 @@
 import axios from "axios";
 
-export async function getPicture(query) {
+export async function getPicture(query, page = 1, per_page = 15) {
 const baseUrl = 'https://pixabay.com/api/';
 const keyApi = '43059548-3adc942b003e7790296d060a7';
 
-const queryParams = new URLSearchParams({
+const queryParams = {
     key: keyApi,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    per_page: 40,
-});
-try { const res =  await fetch(`${baseUrl}?${queryParams}`)
-        if (!res.ok) {
-            throw new Error("error");
-    } 
-    const data = await res.json();
-    console.log(data)
+    page,
+    per_page,
+    };
+try { const res =  await axios.get(`${baseUrl}?${queryParams}`)
+   const data = res.data;
+   console.log(data)
     return data;
 } catch(error) {
-    console.log("error");
+  console.log("error");
     } 
 }
